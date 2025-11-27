c2a833fc0ec91fe90025422c0663ed1a170150d8
Bugra Örnek <bugraornek_41@hotmail.com>
2025-11-27 20:33:43 +0100

    Spring Boot Backend Initial Structure

---
+++ backend/src/main/java/com/bugra/sailfuel/SailfuelApplication.java
@@ -0,0 +1,13 @@
+package com.bugra.sailfuel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SailfuelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SailfuelApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bugra/sailfuel/SailfuelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bugra.sailfuel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SailfuelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
